e7188dae50d529c3884acc85838cacaa46eb26c9
ChristopherOBerry <chroberry@gmail.com>
2019-07-08 02:13:46 -0600

    cert

--- app/components/time/time-service.js
+++ app/components/time/time-service.js
@@ -1,7 +1,7 @@
 import Time from "../../models/time.js";
 
 const timeApi = axios.create({
-	baseURL: 'https://www.worldclockapi.com/api/json/mst/now/',
+	baseURL: 'https://worldclockapi.com/api/json/mst/now/',
 	timeout: 3000
 });
 
