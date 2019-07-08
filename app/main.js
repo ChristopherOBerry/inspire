4a65b83f9a3cfe3d183f91499630482da35b6a38
ChristopherOBerry <chroberry@gmail.com>
2019-07-08 01:33:21 -0600

    paths

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
-import WeatherController from "/./components/weather/weather-controller.js";
-import TodoController from "/./components/todo/todo-controller.js";
-import ImageController from "/./components/image/image-controller.js";
-import QuoteController from "/./components/quote/quote-controller.js";
-import TimeController from "/./components/time/time-controller.js";
-import CardController from "/./components/card/card-controller.js";
+import WeatherController from "./components/weather/weather-controller.js";
+import TodoController from "./components/todo/todo-controller.js";
+import ImageController from "./components/image/image-controller.js";
+import QuoteController from "./components/quote/quote-controller.js";
+import TimeController from "./components/time/time-controller.js";
+import CardController from "./components/card/card-controller.js";
 
 // HEY WHERE ARE ALL OF YOUR CONTROLLERS??
 class App {
