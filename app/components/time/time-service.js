19436f868baa5bf66abd08f5f3c6c60863757820
Chroberry <chroberry@gmail.com>
2019-09-24 11:52:35 -0600

    time fix?

--- app/components/time/time-service.js
+++ app/components/time/time-service.js
@@ -1,40 +1,40 @@
 import Time from "../../models/time.js";
 
 const timeApi = axios.create({
-	baseURL: 'http://worldclockapi.com/api/json/mst/now/',
-	timeout: 10000
+  baseURL: "http://worldclockapi.com/api/json/mst/now/",
+  timeout: 10000
 });
 
 let _state = {
-	time: [],
-	error: []
-	
-}
+  time: [],
+  error: []
+};
 let _subscribers = {
-	time: [],
-	error: []
-}
+  time: [],
+  error: []
+};
 
 function _setState(prop, data) {
-	_state[prop] = data
-	_subscribers[prop].forEach(fn => fn())
-	
+  _state[prop] = data;
+  _subscribers[prop].forEach(fn => fn());
 }
 
-export default class TimeService{
-    get Time(){
-        return _state.time
-	}
-	get TimeError() {
-		return _state.error
-	}
-    addSubscriber(prop, fn) {
-		_subscribers[prop].push(fn)
-    }
-    getTime(){
-        timeApi.get().then(res => {
-			_setState('time', new Time(res.data))
-		})
-		.catch(err => console.error('error', err))
-    }
+export default class TimeService {
+  get Time() {
+    return _state.time;
+  }
+  get TimeError() {
+    return _state.error;
+  }
+  addSubscriber(prop, fn) {
+    _subscribers[prop].push(fn);
+  }
+  getTime() {
+    timeApi
+      .get()
+      .then(res => {
+        _setState("time", new Time(res.data));
+      })
+      .catch(err => console.error("error", err));
+  }
 }
--- app/models/time.js
+++ app/models/time.js
@@ -1,23 +1,20 @@
-export default class Time{
-    constructor(data){
-        this.currentDateTime = data.currentDateTime
-        this.dayOfTheWeek = data.dayOfTheWeek
-        
+export default class Time {
+  constructor(data) {
+    this.currentDateTime = data.currentDateTime;
+    this.dayOfTheWeek = data.dayOfTheWeek;
+  }
 
-        
+  get Template() {
+    let currentTime = this.currentDateTime;
+    let displayMid = currentTime.toString().split("T");
+    let display = displayMid[1].toString();
+    let final = display.split("-");
+    console.log(final[0]);
+    console.log(moment());
 
-        
-    }
-
-    get Template (){
-        let currentTime = this.currentDateTime
-        let displayMid = currentTime.toString().split("T")
-        let display = displayMid[1].toString()
-        let final = display.split("-")
-        console.log(final[0])
-        
-        return `<h4>${this.dayOfTheWeek}</h4>
-        <h4>${final[0]}</h4>
-        `
-    }
+    return `
+        <h3>${moment().format("dddd, MMMM, YYYY")}</h3>
+        <h1>${moment().format("LT")}</h1>
+        `;
+  }
 }
